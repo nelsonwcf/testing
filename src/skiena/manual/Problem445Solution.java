bd5c44d900deb0fd1eda9119b309170f13e3cda6
Nelson Corrocher <nelsonwcf@gmail.com>
2017-12-20 01:18:43 -0500

    - Improved problem 445

--- src/skiena/manual/Problem445Solution.java
+++ src/skiena/manual/Problem445Solution.java
@@ -1,63 +1,44 @@
 package skiena.manual;
 
-import java.util.ArrayList;
-
-class Triplet {
-  private int currentMax;
-  private int currentMin;
-  private int[] currentTriplet = new int[3];
-
-  private int minMax;
-  private int minMin;
-  private int[] minTriplet = new int[3];
-
-  public Triplet() {
-    minMax = 0;
-    minMin = 0;
-    currentMax = 0;
-    currentMin = 0;
-  }
-
-  public void update(int n, int index) {
-    currentTriplet[index] = n;
-    currentMax = Math.max(currentTriplet[0], Math.min(currentTriplet[1], currentTriplet[2]));
-    currentMin = Math.min(currentTriplet[0], Math.min(currentTriplet[1], currentTriplet[2]));
-
-    if (currentDistance() < minDistance()) {
-      minTriplet[0] = currentTriplet[0];
-      minTriplet[1] = currentTriplet[1];
-      minTriplet[2] = currentTriplet[2];
-    }
-  }
-
-  public int currentDistance() {
-    return currentMax - currentMin;
-  }
-
-  public int minDistance() {
-    return minMax - minMin;
-  }
-
-  public int[] getMinTriplet() {
-    return minTriplet;
-  }
-}
-
 public class Problem445Solution {
-  static int getMinSnippet(ArrayList<Integer> wordIndex1,ArrayList<Integer> wordIndex2, ArrayList<Integer> wordIndex3) {
-    if (wordIndex1 == null || wordIndex2 == null || wordIndex3 == null) {
-      return -1;
+  static int[] findSmallestSnipplet(int[] word1, int[] word2, int[] word3) {
+    int i1 = 0;
+    int i2 = 0;
+    int i3 = 0;
+    int[] minSnipplet = new int[3];
+    int minDist = 0;
+
+    minSnipplet[0] = word1[0];
+    minSnipplet[1] = word2[0];
+    minSnipplet[2] = word3[0];
+    minDist = Math.max(word1[0], Math.max(word2[0], word3[0])) - Math.min(word1[0], Math.max(word2[0], word3[0]));
+
+    while(i1 < word1.length - 1 && i2 < word2.length - 1 && i3 < word3.length - 1) {
+      if (word1[i1] <= word2[i2] && word1[i1] <= word3[i3]) {
+        i1++;
+      } else if (word2[i2] <= word1[i1] && word2[i2] <= word3[i3]) {
+        i2++;
+      } else {
+        i3++;
+      }
+      if (Math.max(word1[i1], Math.max(word2[i2], word3[i3])) - Math.min(word1[i1], Math.max(word2[i2], word3[i3])) < minDist) {
+        minDist = Math.max(word1[i1], Math.max(word2[i2], word3[i3])) - Math.min(word1[i1], Math.max(word2[i2], word3[i3]));
+        minSnipplet[0] = word1[i1];
+        minSnipplet[1] = word2[i2];
+        minSnipplet[2] = word3[i3];
+      }
     }
 
-    // Indexes of each wordIndex
-    int i1 = 0, i2 = 0, i3 = 0;
-    int distance = 0;
-    Triplet triplet = new Triplet();
-
-    while (i1 < wordIndex1.size() && i2 < wordIndex2.size() && i3 < wordIndex3.size()) {
-
-    }
+    return minSnipplet;
   }
 
-
+  public static void main(String[] args) {
+    int[] word1 = {1,4,5,11};
+    int[] word2 = {4,9,10};
+    int[] word3 = {1,6,9,12};
+    for(int n : findSmallestSnipplet(word1, word2, word3)) {
+      System.out.print(n + " ");
+    }
+    System.out.println();
+  }
 }
