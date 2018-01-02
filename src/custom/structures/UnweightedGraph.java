4ceb195ef77a69ac0271e02ba787e7fd4a2abf57
Nelson Corrocher <nelsonwcf@gmail.com>
2018-01-02 16:36:35 -0500

    - Added DFS to graph structure.

--- src/custom/structures/UnweightedGraph.java
+++ src/custom/structures/UnweightedGraph.java
@@ -19,33 +19,33 @@ public class UnweightedGraph<T> {
     verticeOutDegree = new HashMap<>();
   }
 
-  public boolean addVertix(T vertix) {
-    if (adjacencyMap.containsKey(vertix)) {
+  public boolean addVertex(T vertex) {
+    if (adjacencyMap.containsKey(vertex)) {
       return false;
     }
 
-    adjacencyMap.put(vertix, new LinkedList<T>());
-    verticeOutDegree.put(vertix, 0);
+    adjacencyMap.put(vertex, new LinkedList<T>());
+    verticeOutDegree.put(vertex, 0);
     numberOfVertices++;
     return true;
   }
 
-  public boolean addEdge(T vertixOfOrigin, T vertixOfDestination) {
-    if (!adjacencyMap.containsKey(vertixOfOrigin)) {
-      addVertix(vertixOfOrigin);
+  public boolean addEdge(T vertexOfOrigin, T vertexOfDestination) {
+    if (!adjacencyMap.containsKey(vertexOfOrigin)) {
+      addVertex(vertexOfOrigin);
     }
 
-    if (!adjacencyMap.containsKey(vertixOfDestination)) {
-      addVertix(vertixOfDestination);
+    if (!adjacencyMap.containsKey(vertexOfDestination)) {
+      addVertex(vertexOfDestination);
     }
 
-    adjacencyMap.get(vertixOfOrigin).add(vertixOfDestination);
-    verticeOutDegree.put(vertixOfOrigin, verticeOutDegree.get(vertixOfOrigin) + 1);
+    adjacencyMap.get(vertexOfOrigin).add(vertexOfDestination);
+    verticeOutDegree.put(vertexOfOrigin, verticeOutDegree.get(vertexOfOrigin) + 1);
     numberOfEdges++;
 
     if (!isDirected) {
-      adjacencyMap.get(vertixOfDestination).add(vertixOfOrigin);
-      verticeOutDegree.put(vertixOfDestination, verticeOutDegree.get(vertixOfDestination) + 1);
+      adjacencyMap.get(vertexOfDestination).add(vertexOfOrigin);
+      verticeOutDegree.put(vertexOfDestination, verticeOutDegree.get(vertexOfDestination) + 1);
       numberOfEdges++;
     }
 
@@ -59,13 +59,13 @@ public class UnweightedGraph<T> {
     }
   }
 
-  boolean bfs(T vertix) {
+  boolean bfs(T vertex) {
     HashSet<T> processed;
     ArrayDeque<T> discovered;
     HashMap<T, Integer> distance;
     int currentDistance;
 
-    if (!adjacencyMap.containsKey(vertix)) {
+    if (!adjacencyMap.containsKey(vertex)) {
       return false;
     }
 
@@ -75,12 +75,12 @@ public class UnweightedGraph<T> {
     distance = new HashMap<>();
     currentDistance = 0;
 
-    // Addfirst Vertix to the queue
-    processed.add(vertix);
-    discovered.addFirst(vertix);
-    distance.put(vertix, currentDistance);
+    // Addfirst Vertex to the queue
+    processed.add(vertex);
+    discovered.addFirst(vertex);
+    distance.put(vertex, currentDistance);
 
-    // Remove vertix, check all neighbors not in the set yet and add those to the queue
+    // Remove vertex, check all neighbors not in the set yet and add those to the queue
     while (!discovered.isEmpty()) {
       T current = discovered.removeLast();
       currentDistance = distance.get(current);
@@ -93,7 +93,7 @@ public class UnweightedGraph<T> {
       }
     }
 
-    // Takes care of graph nodes that are disconnected from vertix
+    // Takes care of graph nodes that are disconnected from vertex
     for (T disjointedNode : adjacencyMap.keySet()) {
       if (!processed.contains(disjointedNode)) {
         processed.add(disjointedNode);
@@ -106,14 +106,39 @@ public class UnweightedGraph<T> {
     return true;
   }
 
+  void dfs(T vertex) {
+    HashSet<T> memory = new HashSet<>();
+
+    dfs(vertex, memory, 0);
+    for (T disjointedNode : adjacencyMap.keySet()) {
+      if (!memory.contains(disjointedNode)) {
+        memory.add(disjointedNode);
+        System.out.print(disjointedNode + ":" + (-1) + " ");
+      }
+    }
+  }
+
+  private void dfs(T vertex, HashSet<T> processed, int distance) {
+    if (!processed.contains(vertex)) {
+      System.out.print(vertex + ":" + distance + " ");
+      processed.add(vertex);
+      for (T v : adjacencyMap.get(vertex)) {
+        dfs(v, processed, distance + 1);
+      }
+    }
+  }
+
   public static void main(String[] args) {
     UnweightedGraph<String> g = new UnweightedGraph<>(false);
-    g.addVertix("A");
-    g.addVertix("B");
-    g.addEdge("B","D");
-    g.addVertix("C");
-    g.addEdge("A","C");
-    g.bfs("A");
+    g.addEdge("A", "B");
+    g.addEdge("A", "C");
+    g.addEdge("A", "D");
+    g.addEdge("B", "E");
+    g.addEdge("D", "F");
+    g.addEdge("D", "G");
+    g.addEdge("G", "H");
+    g.addVertex("X");
+    g.dfs("A");
   }
 }
 
