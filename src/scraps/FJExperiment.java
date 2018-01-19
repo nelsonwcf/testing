759b6f51e87ac9eea3659de4f755230c0e8d2508
Nelson Corrocher <nelsonwcf@gmail.com>
2018-01-19 16:30:45 -0500

    - Added FJExperiment;

---
+++ src/scraps/FJExperiment.java
@@ -0,0 +1,73 @@
+package scraps;
+
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveAction;
+
+class Transform extends RecursiveAction {
+  int seqThreshold;
+  double[] data;
+
+  int start;
+  int end;
+
+  Transform(double[] vals, int s, int e, int t) {
+    data = vals;
+    start = s;
+    end = e;
+    seqThreshold = t;
+  }
+
+  @Override
+  protected void compute() {
+    if ((end - start) < seqThreshold) {
+      for (int i = start; i < end; i++) {
+        if ((data[i] % 2) == 0) {
+          data[i] = Math.sqrt(data[i]);
+        } else {
+          data[i] = Math.cbrt(data[i]);
+        }
+      }
+    } else {
+      int middle = (start + end) / 2;
+      invokeAll(new Transform(data, start, middle, seqThreshold),
+          new Transform(data, middle, end, seqThreshold));
+    }
+  }
+}
+
+public class FJExperiment {
+  public static void main(String[] args) {
+    int pLevel;
+    int threshold;
+
+    if(args.length != 2) {
+      System.out.println("Usage: FJExperiment parallelism threshold ");
+      return;
+    }
+
+    pLevel = Integer.parseInt(args[0]);
+    threshold = Integer.parseInt(args[1]);
+
+    long beginT;
+    long endT;
+
+    ForkJoinPool fjp = new ForkJoinPool(pLevel);
+
+    double[] nums = new double[1000000];
+
+    for (int i = 0; i < nums.length; i++) {
+      nums[i] = (double) i;
+    }
+
+    Transform task = new Transform(nums, 0, nums.length, threshold);
+
+    beginT = System.nanoTime();
+    fjp.invoke(task);
+    endT = System.nanoTime();
+
+    System.out.println("Level of parallelism: " + pLevel);
+    System.out.println("Sequential threshold: " + threshold);
+    System.out.println("Elapsed time: " + (endT - beginT) + " ns");
+    System.out.println();
+  }
+}
