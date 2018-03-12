72886751d79c3f1a1d42bfe65f6b7246bfbd6928
Nelson Corrocher <nelsonwcf@gmail.com>
2018-03-12 01:21:47 -0400

    EvenTreeSolution Update

--- src/hackerrank/EvenTreeSolution.java
+++ src/hackerrank/EvenTreeSolution.java
@@ -3,13 +3,14 @@ package hackerrank;
 import javafx.util.Pair;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Scanner;
 
 public class EvenTreeSolution {
   static class Node {
+    // parent will the first node in the list
     static final int PARENT = 0;
 
+    // pairs will contain the node who is child and total nodes behind that edge
     ArrayList<Pair<Node, Integer>> neighbor;
     int value;
 
@@ -22,16 +23,31 @@ public class EvenTreeSolution {
       this(value);
       neighbor.add(new Pair<>(parent, 0));
     }
+
+    void addNode(Node n) {
+      neighbor.add(new Pair<>(n, 0));
+    }
   }
 
   static class Tree {
+    Node head;
+    int size;
+
+    Tree() {
+      head = null;
+      size = 0;
+    }
+
+    void addEdge(Node parent, Node child) {
+      parent.addNode(child);
+    }
 
   }
 
 
 
   static int evenTree(int n, int m, int[][] tree) {
-
+    return 0;
   }
 
   public static void main(String[] args) {
